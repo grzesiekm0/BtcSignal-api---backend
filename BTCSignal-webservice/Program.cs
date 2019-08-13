1fbc5278e947973f47851e25269118c9a01fb28d
Grzegorz Macko <grzmacko@gmail.com>
2019-08-13 23:22:55 +0200

    Main has been optimalized for sqlite.

--- BTCSignal-webservice/Program.cs
+++ BTCSignal-webservice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using btcsignalwebservice.Model;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +15,20 @@ namespace btcsignal_webservice
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            using (var db = new AlertContext())
+            {
+                db.Alert.Add(new btcsignalwebservice.Alert { Name = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Alert)
+                {
+                    Console.WriteLine(" - {0}", blog.Name);
+                }
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
