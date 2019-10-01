39f21e20f8c8c0b46f2cde9b64be38e96cd6a44e
Grzegorz Macko <grzmacko@gmail.com>
2019-10-01 23:22:59 +0200

    Model fields has been convert to android model.

--- BTCSignal-webservice/Model/Alert.cs
+++ BTCSignal-webservice/Model/Alert.cs
@@ -4,7 +4,11 @@ namespace btcsignalwebservice
     public class Alert
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public string alertId { get; set; }
+        public string exchange { get; set; }
+        public string course { get; set; }
+        public string currency { get; set; }
+        public int enableAlarm { get; set; }
     }
+
 }
