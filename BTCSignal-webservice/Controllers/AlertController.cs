020d61130ec894388ede88136ed89a71ba1b9ad9
Grzegorz Macko <grzmacko@gmail.com>
2019-10-01 22:06:46 +0200

    Code has been refactored for AlertController

--- BTCSignal-webservice/Controllers/AlertController.cs
+++ BTCSignal-webservice/Controllers/AlertController.cs
@@ -17,14 +17,7 @@ namespace btcsignalwebservice.Controllers
         public AlertController(AlertContext context)
         {
             _context = context;
-            /*
-            if (_context.Alert.Count() == 0)
-            {
-                // Create a new TodoItem if collection is empty,
-                // which means you can't delete all TodoItems.
-                _context.Alert.Add(new Alert { Name = "Item1" });
-                _context.SaveChanges();
-            }*/
+            
         }
 
         // GET: api/alert test
