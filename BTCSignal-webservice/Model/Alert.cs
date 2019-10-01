afd07309bcadafdc839f3338d42e8c295a67a2fb
Grzegorz Macko <grzmacko@gmail.com>
2019-10-01 22:11:42 +0200

    Model field type has been changed from int64 to int32

--- BTCSignal-webservice/Model/Alert.cs
+++ BTCSignal-webservice/Model/Alert.cs
@@ -3,7 +3,7 @@ namespace btcsignalwebservice
 {
     public class Alert
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
