c70e5e6dbad0f1862b8a694ca4ef30368adce926
Grzesiek <grzmacko@gmail.com>
2021-03-04 22:57:01 +0100

    Elements which wasnt user any more has been deleted.

--- BTCSignal-webservice/Controllers/AlertController.cs
+++ BTCSignal-webservice/Controllers/AlertController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using btcsignalwebservice.Model;
 
