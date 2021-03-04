175fd8d5895881ba89255785b013ebfc48ef2885
Grzesiek <grzmacko@gmail.com>
2021-03-04 23:05:47 +0100

    First step to implement identity. DbContext, migrations and Startup configurations has been created.

---
+++ BTCSignal-webservice/Model/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace btcsignalwebservice.Model
+{
+    public class AplicationDbContext : IdentityDbContext
+{
+
+    public AplicationDbContext(DbContextOptions options)
+            : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    
+}
+}
--- BTCSignal-webservice/Startup.cs
+++ BTCSignal-webservice/Startup.cs
@@ -5,13 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using btcsignalwebservice.Model;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace btcsignal_webservice
 {
@@ -31,14 +35,46 @@ namespace btcsignal_webservice
             //  opt.UseInMemoryDatabase("AlertList"));
             //services.AddDbContext<AlertContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AlertTestDatabase")));
             //services.AddDbContext<UserContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AlertTestDatabase")));
-            services.AddDbContext<BtcSignalDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AlertTestDatabase")));
+            services.AddDbContext<BtcSignalDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("AlertTestDatabase")));
+            services.AddDbContext<AplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AlertTestDatabase")));
 
+            services.AddIdentity<IdentityUser, IdentityRole>(
+            options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 5;
+            }
+            ).AddEntityFrameworkStores<AplicationDbContext>()
+                .AddDefaultTokenProviders();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["AuthSettings:Audience"],
+                    ValidIssuer = Configuration["AuthSettings:Issuer"],
+                    RequireExpirationTime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["AuthSettings:Key"])),
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+            //services.AddControllersWithViews();
+            //services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -52,7 +88,14 @@ namespace btcsignal_webservice
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(enpoints =>
+            {
+                enpoints.MapControllers();
+            });
+
         }
     }
 }
