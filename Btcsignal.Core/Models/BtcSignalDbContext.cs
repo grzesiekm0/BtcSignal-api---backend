e33cfc6a8b8bf879adc4a576c8767a5a95da349c
Grzesiek <grzmacko@gmail.com>
2021-07-06 19:35:13 +0200

    Dbcontext has been fixed for identity endpoints.

--- Btcsignal.Core/Models/BtcSignalDbContext.cs
+++ Btcsignal.Core/Models/BtcSignalDbContext.cs
@@ -1,11 +1,12 @@
 ﻿
 using Btcsignal.Core.Models.Dao;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Btcsignal.Core.Models
 {
-    public class BtcSignalDbContext : DbContext
-{
+    public class BtcSignalDbContext : IdentityDbContext
+    {
         public BtcSignalDbContext(DbContextOptions<BtcSignalDbContext> options)
                 : base(options)
         {
